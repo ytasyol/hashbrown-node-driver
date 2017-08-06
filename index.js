ba5b12917b1d981428a64fa2015c2509ab994cbf
mrzapp <contact@jeppezapp.com>
2017-08-06 14:33:57 +0200

    Init

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict';
 
-module.exports = require('./HashBrown');
+const Path = require('path');
+const Express = require('express');
+const HashBrown = require('hashbrown-driver');
+const BodyParser = require('body-parser');
+
+const PORT = 8000;
+const APP_ROOT = Path.resolve(__dirname);
+
+// Express
+let app = Express();
+
+// Init HashBrown driver
+HashBrown.init(app);
+
+// Configure express
+app.use(BodyParser.json({limit: '50mb'}));
+app.use(BodyParser.urlencoded({extended: true}));
+app.use(Express.static(APP_ROOT + '/public'));
+app.use('/media', Express.static(APP_ROOT + '/hashbrown/storage/media'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', APP_ROOT + '/views');
+
+// Routes
+app.get('*', (req, res) => {
+    HashBrown.content.getByUrl(req.originalUrl)
+    .then((content) => {
+        res.status(200).render('pages/' + content.template + '.html', { hb: HashBrown, content: content });
+    })
+    .catch((e) => {
+        res.status(404).send(e.stack);
+    });
+});
+
+// Start server
+let server = app.listen(PORT, '0.0.0.0');
